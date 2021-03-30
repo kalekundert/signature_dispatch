9d5cb6b9fc3cfed2deade1165131906c86759f7a
Kale Kundert <kale@thekunderts.net>
2021-03-30 18:36:30 -0400

    test: skip positional-only tests for python<3.8

--- tests/test_dispatch.py
+++ tests/test_dispatch.py
@@ -3,30 +3,6 @@
 import signature_dispatch
 import pytest
 
-def test_positional_only():
-    d = signature_dispatch()
-
-    @d
-    def f(a, /):
-        return a
-    @d
-    def f(a, b, /):
-        return a, b
-
-    assert f(1) == 1
-    assert f(1, 2) == (1, 2)
-
-    with pytest.raises(TypeError):
-        f()
-    with pytest.raises(TypeError):
-        f(1, 2, 3)
-    with pytest.raises(TypeError):
-        f(a=1)
-    with pytest.raises(TypeError):
-        f(1, b=2)
-    with pytest.raises(TypeError):
-        f(a=1, b=2)
-
 def test_positional_or_keyword():
     d = signature_dispatch()
 
